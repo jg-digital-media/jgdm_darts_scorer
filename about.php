945ebf7eb41b39cfa5edefa6a1205ba06102859e
Jonathan Grieve <mail@jonniegrieve.co.uk>
2025-02-28 11:05:38 +0000

    add meta tags

--- about.php
+++ about.php
@@ -1,11 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Darts Scorer App (AI) by Jonnie Grieve Digital Media</title>
+    
+    <meta name="pdf-downloadable" content="yes">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">   
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-N9WB623MVP"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-N9WB623MVP');
+    </script> 
+
+    <!-- Google Font(s) -->
+    <link href="" rel="stylesheet">
+
+    <!-- Meta Tags -->
+    <meta name="description" content="I created this application using Cursor AI and Supermaven for the JavaScript code. The design, the markup and the CSS are my own coding.">
+    <meta name="keywords" content="darts games, darts app, score, scoring, darts scoring darts scoring app, darts scorer, ai, machine learning">
+    <meta name="image" content="htttps://projects.jonniegrieve.co.uk/darts_scorer_app/assets/images/app-image.png">
+
+    <!-- FACEBOOK: Open Graph -->
+    <meta property="og:title" content="About Darts Scorer App (AI) by Jonnie Grieve Digital Media">
+    <meta property="og:description" content="I created this application using Cursor AI and Supermaven for the JavaScript code. The design, the markup and the CSS are my own coding.">
+    <meta property="og:image" content="htttps://projects.jonniegrieve.co.uk/darts_scorer_app/assets/images/app-image.png">
+    <meta property="og:url" content="https://projects.jonniegrieve.co.uk/darts_scorer_app/about.php">
+
+    <!-- TWITTER: Open Graph -->
+    <meta name="twitter:title" content="About Darts Scorer App (AI) by Jonnie Grieve Digital Media">
+    <meta name="twitter:description" content="I created this application using Cursor AI and Supermaven for the JavaScript code. The design, the markup and the CSS are my own coding.">
+    <meta name="twitter:image" content="htttps://projects.jonniegrieve.co.uk/darts_scorer_app/assets/images/app-image.png">
+    <meta name="twitter:card" content="footbal_score_comparator">
+    
+    <!-- Add Favicon -->
+    <link rel="icon" href="favicon.png" type="image/png">
+    
+    <!-- Canonical link -->
+    <link rel="canonical" href="https://projects.jonniegrieve.co.uk/darts_scorer_app/about.php">
+
+    <!-- Stylesheet -->
     <link rel="stylesheet" type="text/css" href="style.css">
+    
+    <!-- Page Title-->
+    <title>About "Darts Scorer App (AI)" by Jonnie Grieve Digital Media</title>
 </head>
 <body>
 
@@ -19,6 +62,8 @@
 
     <section class="about--the--app">
 
+        <h3>About this App</h3>
+
         <p id="app_intro">I created this application using Cursor AI and Supermaven for the JavaScript code. The design, the markup and the CSS are my own coding.
             
         <br /><br />I created a series of blogs to explain the code and the process of creating this app.
@@ -27,9 +72,7 @@
 
         <p>You can track the progress of the app or clone the latest version to your system via <a href="https://github.com/jg-digital-media/jgdm_darts_scorer" target="_blank">GitHub</a>.</p>
         
-        <p><h3>Blogs</h3></p>
-
-        
+        <p><h3>Blogs</h3></p>        
 
         <ul class="blog---list">
 
@@ -62,8 +105,8 @@
             <li>Further darts Darts images were obtained on Freepik <a href="https://www.freepik.com/free-photo/view-dartboard-with-bullseye-arrow-points_42630137.htm">Image by freepik</a></li>
         </ul>
 
-        <p>Cursor AI Chat</p>
-        <p>Supermaven Chat</p>
+        <!-- <p>Cursor AI Chat</p>
+        <p>Supermaven Chat</p> -->
 
         <a href="game.php" class="button" id="btn---game--on">Game On</a>
 
